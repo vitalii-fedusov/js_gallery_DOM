6bb73deb2a91b68fa8422aa93a7db571b3071a01
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-01-17 11:46:16 +0200

    make build

---
+++ index.eef110da.js
@@ -0,0 +1,4 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");// eslint-disable-next-line no-shadow
+e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");t.src=r.href});//# sourceMappingURL=index.eef110da.js.map
+
+//# sourceMappingURL=index.eef110da.js.map
